aba5e0b95e233005f9adace3d091b8c4f0d0e187
Smith, Isaac A <isaac.a.smith@mines.sdsmt.edu>
2018-04-06 14:51:06 -0600

    More setup for data formatting

--- src/learn.py
+++ src/learn.py
@@ -5,16 +5,26 @@ import pandas as pa
 
 def main():
 
+
+    # --- Format the data into a tf usable format ---
+
+
     # Open the credit card data file
     csv = openCSV("creditcard.csv")
+    csv.set_index("Time", inplace = True)
+    
+    # Break the list into two lists, one of fraudulent and one of non-fraudulent
+    fraudList = csv[csv.Class == 1]
+    nonFraudList = csv[csv.Class == 0]
 
-    # Define neural net hyper parameters
-    learning_rate = 0.1
-    epochs = 1
-    batch_size = 10000
+    # Build the list of training data
+    trainList = fraudList.sample(frac = .85)
+    trainList = pa.concat([trainList, nonFraudList.sample(frac = .85)], axis = 0)
 
+    #Build the list of testing data -- anything not in the training data
+    testList = csv.loc[~csv.index.isin(trainList.index)]
 
-    # Set up
+    # --- Set up initial layer weights ---
 
 
     # Define input layer of size 31 (There are 31 values per data point)
@@ -40,7 +50,7 @@ def main():
     Bias4 = tf.Variable(tf.random_normal([1]), name = 'Bias4')
 
 
-    # Set up connections between layers
+    # --- Set up connections between layers ---
 
 
     # Matrix Multiply the input and weights plus the bias for layer 1
@@ -56,6 +66,33 @@ def main():
 
     # Output layer
     output = tf.nn.softmax(tf.add(hidden_out3, Weights4), Bias4)
+    # Normalize output to be between 0 and 1 (ish)
+    normalized = tf.clip_by_value(output, 1e-10, 0.9999999)
+
+    # Define the cost function
+    tmp = tf.placeholder(tf.float32, [None, 2])
+    cost = -tf.reduce_sum(normalized * tf.log(tmp))
+
+    # Define the backpropagation step
+    backprop = tf.train.GradientDescentOptimizer(learning_rate = step_size).minimize(normalized)
+
+    initialize = tf.global_variables_initializer()
+
+
+    # --- Run the learning ---
+
+
+    # Define neural net hyper parameters
+    step_size = 0.1
+    epochs = 1
+    batch_size = 10000
+
+    #with tf.Session() as sess:
+    #    sess.run(initialize)
+
+    #    for epoch in range(epochs):
+    #        avg_cost = 0
+            
 
 
     #http://adventuresinmachinelearning.com/python-tensorflow-tutorial/
