f27927f8287e815576acf1530d879fa1d288a57b
Smith, Isaac A <isaac.a.smith@mines.sdsmt.edu>
2018-04-11 14:53:18 -0600

    More debugging of learning algorithm

--- src/learn.py
+++ src/learn.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python
 
+
+#http://adventuresinmachinelearning.com/python-tensorflow-tutorial/
+#https://www.kaggle.com/mlg-ulb/creditcardfraud
+
+
 import tensorflow as tf
 import pandas as pa
 
 def main():
-    # Define neural net hyper parameters
-    step_size = 0.01
+
+
+    # --- Define neural net hyper parameters ---
+
+
+    step_size = 0.00001
     epochs = 100
     batch_size = 4096
 
     input_layer_width = 30
     layer1_width = 40
     layer2_width = 50
-    layer3_width = 5
+    layer3_width = 60
     output_layer_width = 1
 
     data_count = 284806
@@ -28,8 +37,8 @@ def main():
     fraudList = csv[csv.Class == 1]
     nonFraudList = csv[csv.Class == 0]
 
-    # Build the list of training data
-    trainListInput = fraudList.sample(frac = .85)
+    # Build the list of training data in a random order
+    trainListInput = nonFraudList.sample(frac = .85)
     trainListInput = pa.concat([trainListInput, nonFraudList.sample(frac = .85)], axis = 0)
     trainListInput = trainListInput.sample(frac = 1)
     trainListExpected = trainListInput.Class.as_matrix()
@@ -50,19 +59,19 @@ def main():
     inputLayer = tf.placeholder(tf.float32, [None, input_layer_width])
 
     # Define input -> 1 weights
-    Weights1 = tf.Variable(tf.random_normal([input_layer_width, layer1_width], stddev = .03), name = 'Weights1')
+    Weights1 = tf.Variable(tf.random_normal([input_layer_width, layer1_width], stddev = .02), name = 'Weights1')
     Bias1 = tf.Variable(tf.random_normal([layer1_width]), name = 'Bias1')
 
     # Define layer 1 -> layer 2 weights
-    Weights2 = tf.Variable(tf.random_normal([layer1_width, layer2_width], stddev = .03), name = 'Weights2')
+    Weights2 = tf.Variable(tf.random_normal([layer1_width, layer2_width], stddev = .02), name = 'Weights2')
     Bias2 = tf.Variable(tf.random_normal([layer2_width]), name = 'Bias2')
 
     #Define layer 2 -> layer 3 weights
-    Weights3 = tf.Variable(tf.random_normal([layer2_width, layer3_width], stddev = .03), name = 'Weights3')
+    Weights3 = tf.Variable(tf.random_normal([layer2_width, layer3_width], stddev = .02), name = 'Weights3')
     Bias3 = tf.Variable(tf.random_normal([layer3_width]), name = 'Bias3')
 
     #Define layer 3 -> output weights
-    Weights4 = tf.Variable(tf.random_normal([layer3_width, output_layer_width], stddev = .03), name = 'Weights4')
+    Weights4 = tf.Variable(tf.random_normal([layer3_width, output_layer_width], stddev = .02), name = 'Weights4')
     Bias4 = tf.Variable(tf.random_normal([output_layer_width]), name = 'Bias4')
 
     # Define output layer
@@ -74,18 +83,19 @@ def main():
 
     # Matrix Multiply the input and weights plus the bias for the layer, then RELU it
     hidden_out1 = tf.nn.sigmoid(tf.add(tf.matmul(inputLayer, Weights1), Bias1))
-    hidden_out2 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out1, Weights2), Bias2))
+    hidden_out2 = tf.nn.relu(tf.add(tf.matmul(hidden_out1, Weights2), Bias2))
     hidden_out3 = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out2, Weights3), Bias3))
     output = tf.add(tf.matmul(hidden_out3, Weights4), Bias4)
 
     # Define the cost function
-    cost = -tf.reduce_mean(output - outputLayer)
+    cost = tf.reduce_mean(output - outputLayer) #-tf.losses.mean_squared_error(labels = outputLayer, predictions = output)
 
     # Define the backpropagation step
     backprop = tf.train.GradientDescentOptimizer(learning_rate = step_size).minimize(cost)
 
 
     # --- Run the learning ---
+
 
     forwardOutput = tf.reduce_sum(output)
 
@@ -104,17 +114,14 @@ def main():
                 
                 _, c = sess.run([backprop, cost], feed_dict={inputLayer: inputBatch, outputLayer: expectedBatch})
 
-                asdf, newCost = sess.run([forwardOutput, cost], feed_dict={inputLayer: inputBatch, outputLayer: expectedBatch})
+                out, newCost = sess.run([forwardOutput, cost], feed_dict={inputLayer: inputBatch, outputLayer: expectedBatch})
 
-                avg_cost += c / batchesPerEpoch
+                avg_cost += (c / batchesPerEpoch)
 
-                print("Batch Output: ", asdf, "Batch Cost: ", c)
-
-            print("Epoch: ", (epoch + 1), "Cost: ", avg_cost)
-
-
-    #http://adventuresinmachinelearning.com/python-tensorflow-tutorial/
-    #https://www.kaggle.com/mlg-ulb/creditcardfraud
+                print("Batch Output: ", out, "Batch Cost: ", c)
+            print("------------------------------------------------------------")
+            print("Epoch Number: ", (epoch + 1), "      Epoch Cost: ", avg_cost)
+            print("------------------------------------------------------------")
 
 
 def openCSV(filename):
